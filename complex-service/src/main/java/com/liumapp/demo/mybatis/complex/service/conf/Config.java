78224188c66407f9290d376f84e9d80143253d99
liumapp <liumapp.com@gmail.com>
2018-06-15 11:03:59 +0800

    update package naem

--- complex-service/src/main/java/com/liumapp/demo/mybatis/complex/service/conf/Config.java
+++ complex-service/src/main/java/com/liumapp/demo/mybatis/complex/service/conf/Config.java
@@ -1,4 +1,4 @@
-package com.liumapp.demo.mybatis.complex;
+package com.liumapp.demo.mybatis.complex.service.conf;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
