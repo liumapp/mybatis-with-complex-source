b5def10310302d083b5cc0cfaf71f6faa3a6947d
liumapp <liumapp.com@gmail.com>
2018-06-17 18:51:02 +0800

    using iview radio

--- complex-ui/src/components/add-user.vue
+++ complex-ui/src/components/add-user.vue
@@ -8,7 +8,22 @@
 <template>
 <div>
   <Form ref="addUserForm" :model="addUserModel">
-    
+    <FormItem label="姓名" prop="name">
+      <Input type="text" v-model="addUserModel.name"></Input>
+    </FormItem>
+    <FormItem label="年龄" prop="age">
+      <Input type="text" v-model="addUserModel.age"></Input>
+    </FormItem>
+    <FormItem  label="性别" prop="sex">
+      <RadioGroup  v-model="addUserModel.sex">
+        <Radio label="男"></Radio>
+        <Radio label="女"></Radio>
+      </RadioGroup>
+    </FormItem>
+    <FormItem>
+      <Button type="primary" @click="handleSubmit('checkMsgForm')">Submit</Button>
+      <Button type="ghost" @click="handleReset('checkMsgForm')">Reset</Button>
+    </FormItem>
   </Form>
 </div>
 </template>
@@ -19,7 +34,7 @@ export default {
   data () {
     return {
       addUserModel: {
-        name: '',
+        name: '张三',
         age: 18,
         sex: '男'
       },
@@ -28,6 +43,24 @@ export default {
         age: [],
         sex: []
       }
+    }
+  },
+  methods: {
+    handleSubmit (name) {
+      this.$refs[name].validate((valid) => {
+        if (valid) {
+          util.post('/info', this.checkMsgForm).then(res => {
+            this.$Message.success('Success!');
+            this.$emit('setFormData', this.checkMsgForm);
+            this.$emit('next');
+          });
+        } else {
+          this.$Message.error('Fail!');
+        }
+      });
+    },
+    handleReset (name) {
+      this.$refs[name].resetFields();
     }
   }
 }
