59408380ebe8c67d3ae3cc99c773b088ba60f7b2
liumapp <liumapp.com@gmail.com>
2018-06-17 16:24:03 +0800

    add order and result component

---
+++ complex-ui/src/components/add-order.vue
@@ -0,0 +1,17 @@
+/**
+ * @file add-order.vue
+ * @author liumapp
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 6/17/18
+ */
+<template>
+<div>
+  this is add order component
+</div>
+</template>
+<script>
+export default {
+  name: 'add-order'
+}
+</script>
---
+++ complex-ui/src/components/result-list.vue
@@ -0,0 +1,17 @@
+/**
+ * @file result-list.vue
+ * @author liumapp
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 6/17/18
+ */
+<template>
+<div>
+  this is result list component
+</div>
+</template>
+<script>
+export default {
+  name: 'result-list'
+}
+</script>
