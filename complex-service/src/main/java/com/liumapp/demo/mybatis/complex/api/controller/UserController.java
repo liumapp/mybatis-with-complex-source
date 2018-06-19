5ab20e0ff7ef7fdc382951c7bc4d93bf85b63430
liumapp <liumapp.com@gmail.com>
2018-06-19 11:40:18 +0800

    add user done

--- complex-service/src/main/java/com/liumapp/demo/mybatis/complex/api/controller/UserController.java
+++ complex-service/src/main/java/com/liumapp/demo/mybatis/complex/api/controller/UserController.java
@@ -1,6 +1,10 @@
 package com.liumapp.demo.mybatis.complex.api.controller;
 
 import com.alibaba.fastjson.JSON;
+import com.liumapp.demo.mybatis.complex.api.testa.domain.User;
+import com.liumapp.demo.mybatis.complex.api.testa.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,8 +19,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("user")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
     @RequestMapping("/")
-    public String addUser () {
+    public String addUser (@RequestBody User user) {
+        userService.insert(user);
         return JSON.toJSONString("success");
     }
 
--- complex-service/src/main/java/com/liumapp/demo/mybatis/complex/api/testa/service/impl/UserServiceImpl.java
+++ complex-service/src/main/java/com/liumapp/demo/mybatis/complex/api/testa/service/impl/UserServiceImpl.java
@@ -1,9 +1,13 @@
 package com.liumapp.demo.mybatis.complex.api.testa.service.impl;
 
 import com.liumapp.demo.mybatis.complex.api.testa.domain.User;
+import com.liumapp.demo.mybatis.complex.api.testa.mapper.UserMapper;
 import com.liumapp.demo.mybatis.complex.api.testa.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.io.Serializable;
 
 /**
  * @author liumapp
@@ -16,9 +20,12 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class UserServiceImpl implements UserService {
 
+    @Autowired
+    private UserMapper userMapper;
+
     @Override
     public int insert(User user) {
-        return 0;
+        return userMapper.insert(user);
     }
 
 }
