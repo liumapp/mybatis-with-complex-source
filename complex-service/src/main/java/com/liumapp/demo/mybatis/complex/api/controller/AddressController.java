ec50ff7cf5b61a26e5659c31c48ee5cf7c4ba3bd
liumapp <liumapp.com@gmail.com>
2018-06-19 14:18:42 +0800

    add order controller

--- complex-service/src/main/java/com/liumapp/demo/mybatis/complex/api/controller/AddressController.java
+++ complex-service/src/main/java/com/liumapp/demo/mybatis/complex/api/controller/AddressController.java
@@ -2,6 +2,8 @@ package com.liumapp.demo.mybatis.complex.api.controller;
 
 import com.alibaba.fastjson.JSON;
 import com.liumapp.demo.mybatis.complex.api.testa.domain.ShippingAddress;
+import com.liumapp.demo.mybatis.complex.api.testa.service.ShippingAddressService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,8 +19,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("address")
 public class AddressController {
 
+    @Autowired
+    private ShippingAddressService shippingAddressService;
+
     @RequestMapping("/add")
     public String addAddress (@RequestBody ShippingAddress shippingAddress) {
+        shippingAddressService.insert(shippingAddress);
         return JSON.toJSONString("success");
     }
 
---
+++ complex-service/src/main/java/com/liumapp/demo/mybatis/complex/api/controller/OrderController.java
@@ -0,0 +1,19 @@
+package com.liumapp.demo.mybatis.complex.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author liumapp
+ * @file OrderController.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 6/19/18
+ */
+@RestController
+@RequestMapping("order")
+public class OrderController {
+
+
+
+}
