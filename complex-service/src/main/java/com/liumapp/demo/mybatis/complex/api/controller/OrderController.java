b3934010fa8471a5f4cb2133ffe2e2381e6faeb8
liumapp <liumapp.com@gmail.com>
2018-06-19 14:22:39 +0800

    need get order detail

--- complex-service/src/main/java/com/liumapp/demo/mybatis/complex/api/controller/OrderController.java
+++ complex-service/src/main/java/com/liumapp/demo/mybatis/complex/api/controller/OrderController.java
@@ -2,6 +2,8 @@ package com.liumapp.demo.mybatis.complex.api.controller;
 
 import com.alibaba.fastjson.JSON;
 import com.liumapp.demo.mybatis.complex.api.testb.domain.Order;
+import com.liumapp.demo.mybatis.complex.api.testb.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,8 +19,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("order")
 public class OrderController {
 
+    @Autowired
+    private OrderService orderService;
+
     @RequestMapping("/add")
     public String addOrder (@RequestBody Order order) {
+        orderService.insert(order);
+        return JSON.toJSONString("success");
+    }
+
+    @RequestMapping("/detail")
+    public String getOrderDetail (@RequestBody Order order) {
         return JSON.toJSONString("success");
     }
 
