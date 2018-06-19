80ea89219b11a60155105a7e846f09bacd7620e6
liumapp <liumapp.com@gmail.com>
2018-06-19 14:20:12 +0800

    add order service

--- complex-service/src/main/java/com/liumapp/demo/mybatis/complex/api/controller/OrderController.java
+++ complex-service/src/main/java/com/liumapp/demo/mybatis/complex/api/controller/OrderController.java
@@ -1,5 +1,8 @@
 package com.liumapp.demo.mybatis.complex.api.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.liumapp.demo.mybatis.complex.api.testb.domain.Order;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("order")
 public class OrderController {
 
-
+    @RequestMapping("/add")
+    public String addOrder (@RequestBody Order order) {
+        return JSON.toJSONString("success");
+    }
 
 }
---
+++ complex-service/src/main/java/com/liumapp/demo/mybatis/complex/api/testb/service/OrderService.java
@@ -0,0 +1,16 @@
+package com.liumapp.demo.mybatis.complex.api.testb.service;
+
+import com.liumapp.demo.mybatis.complex.api.testb.domain.Order;
+
+/**
+ * @author liumapp
+ * @file OrderService.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 6/19/18
+ */
+public interface OrderService {
+
+    int insert (Order order);
+
+}
