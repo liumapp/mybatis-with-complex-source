a0f2eb1a2caaa8897b6a5d1c6db6ceb3cdfc7be6
liumapp <liumapp.com@gmail.com>
2018-06-19 10:30:59 +0800

    should use two mybatis mapper scan configuration

---
+++ complex-service/src/main/java/com/liumapp/demo/mybatis/complex/api/conf/MybatisTestaConfig.java
@@ -0,0 +1,19 @@
+package com.liumapp.demo.mybatis.complex.api.conf;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author liumapp
+ * @file MybatisConfig.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 6/19/18
+ */
+@Configuration
+@MapperScan(basePackages = {""})
+public class MybatisTestaConfig {
+
+
+
+}
--- complex-service/src/main/java/com/liumapp/demo/mybatis/complex/api/conf/MybatisTestbConfig.java
+++ complex-service/src/main/java/com/liumapp/demo/mybatis/complex/api/conf/MybatisTestbConfig.java
@@ -4,13 +4,13 @@ import org.springframework.context.annotation.Configuration;
 
 /**
  * @author liumapp
- * @file MybatisConfig.java
+ * @file MybatisTestbConfig.java
  * @email liumapp.com@gmail.com
  * @homepage http://www.liumapp.com
  * @date 6/19/18
  */
 @Configuration
-public class MybatisConfig {
+public class MybatisTestbConfig {
 
 
 
