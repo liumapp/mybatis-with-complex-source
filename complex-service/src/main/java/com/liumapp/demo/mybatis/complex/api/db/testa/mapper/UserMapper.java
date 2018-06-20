d242ba3a723cff283cd9f0da067e61cef8d3fa6c
liumapp <liumapp.com@gmail.com>
2018-06-20 11:35:28 +0800

    add mybatis domain and mapper back

---
+++ complex-service/src/main/java/com/liumapp/demo/mybatis/complex/api/db/testa/mapper/UserMapper.java
@@ -0,0 +1,30 @@
+package com.liumapp.demo.mybatis.complex.api.db.testa.mapper;
+
+import com.liumapp.demo.mybatis.complex.api.db.testa.domain.User;
+import com.liumapp.demo.mybatis.complex.api.db.testa.domain.UserExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface UserMapper {
+    long countByExample(UserExample example);
+
+    int deleteByExample(UserExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    List<User> selectByExample(UserExample example);
+
+    User selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByExample(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
